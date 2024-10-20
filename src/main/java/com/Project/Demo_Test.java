43119d3fe34f4592d966f12cbe40d329960d55da
chavanSavita34 <chavansavita431@gmail.com>
2024-10-20 10:24:24 -0700

    updated test case 1

--- src/main/java/com/Project/Demo_Test.java
+++ src/main/java/com/Project/Demo_Test.java
@@ -5,6 +5,9 @@ public class Demo_Test {
 	public static void main(String[] args) {
       
 		System.out.println("Welcome To BugSpotter");
+		
+		System.out.println("Welcome To BugSpotter");
+
 	}
 
 }
